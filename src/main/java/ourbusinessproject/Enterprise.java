6d9b86835f94c97ac593eb93d14e734aa92593bf
Enzo Deher <enzo.deher@etu.iut-tlse3.fr>
2026-01-27 11:46:35 +0100

    fix #1.3 Mapping Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,52 +1,94 @@
 package ourbusinessproject;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name = "Enterprise")
 public class Enterprise {
 
-    @NotNull
-    @Size(min = 1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @NotBlank
+    @Column(name = "Name")
     private String Name;
+
     @Size(min = 10)
+    @Column(name = "Description")
     private String Description;
-    @NotNull
-    @Size(min = 1)
+
+    @NotBlank
+    @Column(name = "ContactName")
     private String ContactName;
-    @NotNull
-    @Size(min = 1)
-    @Email
+
+    @NotBlank @Email
+    @Column(name = "ContactEmail")
     private String ContactEmail;
 
+    /**
+     *
+     * @return the name
+     */
     public String getName() {
         return Name;
     }
 
+    /**
+     * Set the name
+     * @param name the name
+     */
     public void setName(String name) {
         Name = name;
     }
 
+    /**
+     *
+     * @return the description
+     */
     public String getDescription() {
         return Description;
     }
 
+    /**
+     * Set the description
+     * @param description the description
+     */
     public void setDescription(String description) {
         Description = description;
     }
 
+    /**
+     *
+     * @return the contactName
+     */
     public String getContactName() {
         return ContactName;
     }
 
+    /**
+     * Set the contactName
+     * @param contactName the contactName
+     */
     public void setContactName(String contactName) {
         ContactName = contactName;
     }
 
+    /**
+     *
+     * @return the contactEmail
+     */
     public String getContactEmail() {
         return ContactEmail;
     }
 
+    /**
+     * Set the contactEmail
+     * @param contactEmail the contactEmail
+     */
     public void setContactEmail(String contactEmail) {
         ContactEmail = contactEmail;
     }
--- src/main/java/ourbusinessproject/Project.java
+++ src/main/java/ourbusinessproject/Project.java
@@ -1,27 +1,51 @@
 package ourbusinessproject;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "Project")
 public class Project {
 
-    @NotNull
-    @Size(min=1)
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @NotBlank
+    @Column(name = "Title")
     private String Title;
+
+    @Column(name = "Description")
     private String Description;
 
+    /**
+     *
+     * @return the description
+     */
     public String getDescription() {
         return Description;
     }
 
+    /**
+     *
+     * @return the title
+     */
     public String getTitle() {
         return Title;
     }
 
+    /**
+     * Set the title
+     * @param title the title
+     */
     public void setTitle(String title) {
         Title = title;
     }
 
+    /**
+     * Set the description
+     * @param description the description
+     */
     public void setDescription(String description) {
         Description = description;
     }
